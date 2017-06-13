30eb4f8a2cb78e5be2f7088a09fb603d09282c43
Víctor Velarde <victor.velarde@gmail.com>
2017-06-14 00:00:55 +0200

    Basic game is ready

--- src/Board.js
+++ src/Board.js
@@ -12,15 +12,17 @@ class Board extends Component {
         null, null, null,
         null, null, null
       ],
-      next: 'X'
+      next: 'X',
+      winner: null
     };
   }
 
   render() {
+    let w = this.isThereAWinner();
 
     return (
       <div>
-        <div className="status">next player is {this.state.next}</div>
+        {this.renderStatus(w)}
 
         <div className="board-row">
           {this.renderSquare(0)}
@@ -37,16 +39,52 @@ class Board extends Component {
           {this.renderSquare(7)}
           {this.renderSquare(8)}
         </div>
+
       </div>
     ); //squares;
   }
 
+  renderStatus(winner){
+    if (winner){
+      return (<span> `The winner is ` {winner}</span>);
+    }
+    return (
+      <div className="status">next player is {this.state.next}</div>);
+  }
+
   handleClick(i){
     let s = this.state.squares.slice();
+
+    let noMore = this.isThereAWinner() || s[i] !== null;
+    if (noMore) return;
+
     let current = this.state.next;
     s[i] = current;
     let n = (current === 'X') ? 'O' : 'X';
     this.setState({squares: s, next: n});
+  }
+
+  isThereAWinner(){
+    const winCombinations = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [6, 4, 2]
+    ];
+
+    let s = this.state.squares;
+    for(let three of winCombinations){
+      let [a, b, c] = three;
+      if (a && s[a] === s[b] && s[b] === s[c]){
+          let winner = s[a];
+          return winner;
+      }
+    }
+    return null;
   }
 
   renderSquare(i) {
